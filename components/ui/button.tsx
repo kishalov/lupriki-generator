eac9b251bb114459f4e5bf8e571704ea51ddcb7e
Nikolai <kishalovn@gmail.com>
2025-11-23 23:30:57 +0400

    btn fix

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -12,11 +12,12 @@ export function Button({ className, tooltip, ...props }: ButtonProps) {
 	return (
 		<button
 			{...props}
+			style={{ touchAction: "manipulation" }} // Отключает shrink на телефонах
 			className={cn(
 				"relative",
 				"btn-9 font-bold uppercase text-[16px] flex items-center justify-center w-full text-[#fb6950] select-none transition-all duration-150 group",
 				"hover:btn-9-hover active:btn-9-active disabled:opacity-35",
-				"btn-children-press",
+				// УБИРАЕМ: "btn-children-press",
 				"text-shadow-[0_-1px_0_#000,0_1px_0_#000,1px_0_0_#000,-1px_0_0_#000]",
 				"p-2",
 				"[&_img]:w-10 [&_img]:h-10",
@@ -25,18 +26,9 @@ export function Button({ className, tooltip, ...props }: ButtonProps) {
 		>
 			{props.children}
 
-			{/* Тултип */}
 			{tooltip && (
 				<span
-					className="
-						absolute left-1/2 -translate-x-1/2 bottom-[calc(100%)]
-						opacity-0 pointer-events-none
-						translate-y-2
-						transition-all duration-200
-						dialog-9
-						group-hover:opacity-100 group-hover:translate-y-0
-						text-lg text-shadow-none text-black leading-5 font-normal
-					"
+					className="absolute left-1/2 -translate-x-1/2 bottom-[calc(100%)] opacity-0 pointer-events-none translate-y-2 transition-all duration-200 dialog-9 group-hover:opacity-100 group-hover:translate-y-0 text-lg text-shadow-none text-black leading-5 font-normal"
 				>
 					{tooltip}
 				</span>
